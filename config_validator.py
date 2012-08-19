a4b81e907ad125094c013eda68b6d1826aeaa77d
Akarsh E S <akarsh.es@gmail.com>
2012-08-19 16:51:35 +0530

    Implemented argparse for config file paths. Added basic testing

--- config_validator.py
+++ config_validator.py
@@ -1,6 +1,9 @@
 from jinja2 import Template
 import re
 import test_parser
+import argparse
+import os
+import sys
 
 def render_template(data):
     """Renders a template using the data passed to it"""
@@ -18,14 +21,31 @@ def render_template(data):
     for key in keys:
         print template.render(title = key, kvps = data[key])
 
-#def compare(d1,d2):
-#    if d1 == d2: return 0
-#    elif len(d1) is not len(d1):
+def compare(old,new):
+    mod = old
+    if len(old) is not len(new):
+        temp = new.keys()
+        for key in old.keys():
+            temp.pop(temp.index(key))
+        print("The following blocks are missing in your config.\n Please select an option for changing the configuratoin to the optimal value. [Y/n]")
+        print(temp)
+        dec = str(raw_input())
 
+    
 
 if __name__ == "__main__":
     global data
-    data1 = test_parser.parse("/home/akarsh/Documents/tmp/config2.conf")
-    data2 = test_parser.parse("/home/akarsh/Documents/git/NetApp-PESIT-Configuration-Validation-Tool/output/output.conf")
+    parser = argparse.ArgumentParser(description = "Accepts addresses of config file")
+    parser.add_argument("--path1",dest = "path1")
+    parser.add_argument("--path2",dest = "path2")
+    paths = parser.parse_args()
+    if paths.path1 is None or paths.path2 is None:
+        print("Paths incomplete")
+        sys.exit()
+    if not os.path.exists(paths.path1) or not os.path.exists(paths.path2):
+        print("Files do not exist.")
+        sys.exit()
+    data1 = test_parser.parse(paths.path1)#"/home/akarsh/Documents/tmp/config2.conf")
+    data2 = test_parser.parse(paths.path2)#"/home/akarsh/Documents/git/NetApp-PESIT-Configuration-Validation-Tool/output/output.conf")
     render_template(data1)
     print data1 == data2
